8d45dc3bbd4b82c616c81b477f3b437c2c34faad
akky25 <ry.akita1991@gmail.com>
2021-03-17 18:38:53 +0900

    props 追加

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,31 @@ const Header: FC = () => (
   </header>
 );
 
-const Image: FC = () => (
+type imageProps = {
+  src: string;
+};
+
+const Image: FC<imageProps> = ({ src }) => (
   <div className="card">
     <div className="card-image">
       <figure className="image">
-        <img
-          src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-          alt="cute dog"
-        />
+        <img src={src} alt="cute dog" />
       </figure>
     </div>
   </div>
 );
 
-const Gallery: FC = () => (
-  <div className="columns is-vcentered is-multiline">
-    <div className="column is-3">
-      <Image />
+const Gallery: FC = () => {
+  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+
+  return (
+    <div className="columns is-vcentered is-multiline">
+      <div className="column is-3">
+        <Image src={url} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Main: FC = () => (
   <main>
